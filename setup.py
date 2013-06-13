fa7afb90eba3a6bd2c4102647013a11cf7614039
Olivier Grisel <olivier.grisel@ensta.org>
2013-06-13 16:47:47 +0200

    Bump master version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="pyrallel",
-    version="0.1-dev",
+    version="0.3-dev",
     description="Experimental tools for parallel machine learning",
     maintainer="Olivier Grisel",
     mainainer_email="olivier.grisel@ensta.org",
